8e26c935282ed6eba2c5e6f2e346e2530c397731
Carlos <liqsquid00@gmail.com>
2017-05-02 15:13:23 -0300

    Versao 1.0.3

--- Modelo relatorio/imagens/Criarimagens.py
+++ Modelo relatorio/imagens/Criarimagens.py
@@ -1,14 +1,40 @@
+#!/usr/bin/python
+
 import os
+
+filelist=[]
+ignore=[]
+
 for filename in os.listdir(os.getcwd()):
-	if(filename != "Criarimagens.py"):
-		if(filename != "imagens.tex"):
-			if(filename != "criar.sh"):
-				print("%%Imagem "+filename)
-				print("\\begin{center}")
-				print("\\resizebox{1\\columnwidth}{!}{\\includegraphics{{imagens/"+filename+"}}}")
-				print("\\begingroup")
-				print("\\fontsize{7pt}{10pt}\\selectfont")
-				print("\\\\\\textit{Nome da figura}: descricao da figura\\\\ ")
-				print("\\endgroup")
-				print("\\end{center}")
-				print("  ")
+  filelist.append(filename)  
+
+try:
+    with open('ignore', 'r') as f2:  
+        b = f2.readlines()
+    for line in b:
+        ignore.append(line.strip())
+    f2.close()
+except OSError:
+    print "Error"
+    pass
+
+non_duplicates = set(filelist).difference(set(ignore))
+non_duplicates = filter(None, non_duplicates)
+
+listaimagens = open('imagens-snippets.tex', 'w')
+
+for item in non_duplicates:
+	print>>listaimagens, "%%Imagem "+item
+	print>>listaimagens, "\\begin{figure}[h]"
+	print>>listaimagens, "\\resizebox{0.5\\columnwidth}{!}{\\includegraphics{imagens/"+item+"}}"
+	print>>listaimagens, "\\caption{Legenda}"
+	print>>listaimagens, "\\label{fig:"+item+"}"
+	print>>listaimagens, "\\end{figure}"
+	print>>listaimagens, "  "
+
+listaimagens.close()
+
+
+
+
+
--- Modelo relatorio/imagens/criar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python2.7 Criarimagens.py > imagens.tex
